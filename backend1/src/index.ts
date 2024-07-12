61e15c7c63553b038e7dc0f8c443e9971545b75f
Dipti Kumari <diptiku2002@gmail.com>
2024-07-12 12:54:51 +0530

    CHESS UI added

--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -2,16 +2,15 @@
 import { WebSocketServer } from 'ws';
 import { GameManager } from './GameManager';
 
-
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 3000 });
 const gameManager = new GameManager();
-wss.on('connection', function connection(ws){
-    ws.on('message', function incoming(message){
-        console.log('received: %s', message);
-    });
-    gameManager.addUser(ws);
-    ws.send('Welcome on the chess server');
-    ws.on('disconnect', () => {
-        gameManager.removeUser(ws);
-    });
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+  gameManager.addUser(ws);
+  ws.send('Welcome on the chess server');
+  ws.on('disconnect', () => {
+    gameManager.removeUser(ws);
+  });
 });
--- frontend/src/Components/ChessBoard.tsx
+++ frontend/src/Components/ChessBoard.tsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function ChessBoard() {
-  return <div>ChessBoard</div>;
-}
+import { Color, PieceSymbol, Square } from 'chess.js';
+import { MOVE } from '../Screens/Game';
+
+export const ChessBoard = ({
+  socket,
+  setBoard,
+  chess,
+  board,
+}: {
+  chess: any;
+  setBoard: any;
+
+  board: ({
+    square: Square;
+    type: PieceSymbol;
+    color: Color;
+  } | null)[][];
+  socket: WebSocket;
+}) => {
+  const [from, setFrom] = useState<null | Square>(null);
+  // const [to, setTo] = useState<null | Square>(null);
+  return (
+    <div className="grid grid-rows-8 gap-0">
+      {board.map((row, i) => (
+        <div key={i} className="flex flex-row">
+          {row.map((square, j) => {
+            const squareRepresentation = (String.fromCharCode(97 + (j % 8)) + '' + (8 - i)) as Square;
+            return (
+              <div
+                onClick={() => {
+                  if (!from) {
+                    setFrom(squareRepresentation);
+                  } else {
+                    socket.send(
+                      JSON.stringify({
+                        type: MOVE,
+                        payload: {
+                          from,
+                          to: squareRepresentation,
+                        },
+                      })
+                    );
+                    setFrom(null);
+                    chess.move({
+                      from,
+                      to: squareRepresentation,
+                    });
+                    setBoard(chess.board());
+                    console.log({ from, to: squareRepresentation });
+                  }
+                }}
+                key={j}
+                className={`w-12 h-12 flex justify-center items-center ${
+                  (i + j) % 2 === 0 ? 'bg-gray-400' : 'bg-gray-200'
+                }`}
+              >
+                {square ? (
+                  <div className={`text-3xl text-${square.color === 'w' ? 'white' : 'black'}`}>{square.type}</div>
+                ) : null}
+              </div>
+            );
+          })}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ChessBoard;
